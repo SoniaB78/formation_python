87c6dfabcf3e70bce835d8b4885aaf41772a4514
Sonia <bougamha.sonia@gmail.com>
2020-01-13 15:07:34 +0100

    creation fichier pendu
    Fichier pour rassembler le code et qu'il soit organisé.

---
+++ pendu_git/Pendu.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrateur
+#
+# Created:     13/01/2020
+# Copyright:   (c) Administrateur 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class Pendu:
+    def __init__ (self):
