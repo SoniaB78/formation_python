393f21b0bc0c4cbe589b6484852bf86ddeba401f
Florent LANNOIS <florent.lannois@gmail.com>
2020-01-15 12:32:09 +0100

    Creation de la branche de Florent

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-from tkinter import *
-
-fenetre = Tk()
-
-
-alphabet = [chr(n) for n in range(65,65+26)]
-print(alphabet)
-
-
-
-
-
-for i in range(26):
-    print("\tdef lettreTappee{}():\n\t\tprint('{}')\n\t\treturn '{}'".format(chr(65+i),chr(65+i),chr(65+i)))
+'TEST' 
